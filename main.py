c6e411aa2873067aa442e374e8397efb6e4b7574
Greg Vdb <vdb_g@hotmail.com>
2021-12-05 17:05:12 -0800

    add config file

--- main.py
+++ main.py
@@ -1,53 +1,66 @@
 #!/bin/python3
 
-# https://github.com/python-telegram-bot/python-telegram-bot
+import logging
+from pathlib import Path
 
+# https://github.com/python-telegram-bot/python-telegram-bot
 from telegram import Update
 from telegram.ext import CallbackContext
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
-import logging
-
-updater = Updater(token='2135064458:AAEpefZu2GKhp_FupW9BLQd6uf-QS0cYTtg', use_context=True)
-
-dispatcher = updater.dispatcher
-
+import yaml 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                      level=logging.INFO)
 
-
 def start(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
 def echo(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
-
 
 def caps(update: Update, context: CallbackContext):
     text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
-
-
 def unknown(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command.")
 
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
+def main(conf: dict):
+    updater = Updater(token=conf["token"], use_context=True)
+    dispatcher = updater.dispatcher
 
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    dispatcher.add_handler(echo_handler)
 
-updater.idle()
+    caps_handler = CommandHandler('caps', caps)
+    dispatcher.add_handler(caps_handler)
+
+    unknown_handler = MessageHandler(Filters.command, unknown)
+    dispatcher.add_handler(unknown_handler)
+
+    updater.start_polling()
+    updater.idle()
+
+try:
+    with open(Path(__file__).with_name("config.yaml")) as config_file:
+        config = yaml.safe_load(config_file)
+
+        if "telegram" not in config:
+            raise ValueError("Invalid config.yaml. Section telegram required.")
+        
+        if "main" in config:
+            conf = config["main"]
+            #logging.getLogger().setLevel(logging.getLevelName(conf["logverbosity"]))
+
+        main(config["telegram"])
+        exit(0)
+
+except FileNotFoundError as e:
+    logging.error("Missing config.yaml file.")
+    exit(2)
